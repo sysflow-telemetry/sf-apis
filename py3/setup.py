e41fa4b0da1a7f44f11bd41c2ee8668ca3bd550e
Frederico Araujo <frederico.araujo@ibm.com>
2019-08-14 02:27:42 +0000

    added required dependencies

--- py3/setup.py
+++ py3/setup.py
@@ -1,14 +1,13 @@
 import os
-
 from setuptools import setup
 
 setup(
-    name = 'SysFlow Processing API',
+    name = 'sysflow',
     version = '0.1',    
     description = ('Install SysFlow python API and utilities'),    
     packages=['sysflow'],
     package_data={'sysflow': ['schema.avsc']},
-    install_requires=['avro-python3==1.8.2', 'avro-gen==0.3.0'],
+    install_requires=['avro-python3==1.8.2', 'avro-gen==0.3.0', 'tabulate==0.8.3', 'minio==4.0.18'],
     scripts=['utils/sysprint'],
     package_dir = {'': 'classes'}
 )
