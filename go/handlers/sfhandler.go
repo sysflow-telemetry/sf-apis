31db13c404a06bf7d5934c4b8b539d2ba12c37ea
Frederico Araujo <frederico.araujo@ibm.com>
2020-03-29 00:17:31 -0400

    golang api stubs

---
+++ go/handlers/sfhandler.go
@@ -0,0 +1,17 @@
+package handlers
+
+import (
+	"github.ibm.com/sysflow/sfgo"
+)
+
+type SFHandler interface {
+	IsEntityEnabled() bool
+	HandleHeader(hdr *sfgo.SFHeader) error
+	HandleContainer(hdr *sfgo.SFHeader, cont *sfgo.Container) error
+	HandleProcess(hdr *sfgo.SFHeader, cont *sfgo.Container, proc *sfgo.Process) error
+	HandleFile(hdr *sfgo.SFHeader, cont *sfgo.Container, file *sfgo.File) error
+	HandleNetFlow(hdr *sfgo.SFHeader, cont *sfgo.Container, proc *sfgo.Process, nf *sfgo.NetworkFlow) error
+	HandleFileFlow(hdr *sfgo.SFHeader, cont *sfgo.Container, proc *sfgo.Process, file *sfgo.File, ff *sfgo.FileFlow) error
+	HandleFileEvt(hdr *sfgo.SFHeader, cont *sfgo.Container, proc *sfgo.Process, file1 *sfgo.File, file2 *sfgo.File, fe *sfgo.FileEvent) error
+	HandleProcEvt(hdr *sfgo.SFHeader, cont *sfgo.Container, proc *sfgo.Process, pe *sfgo.ProcessEvent) error
+}
