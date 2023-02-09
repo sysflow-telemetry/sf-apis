e4f38e6c23515754c806e51edad70a55b08fa30a
Frederico Araujo <frederico.araujo@ibm.com>
2023-02-08 21:10:54 -0500

    feat(go): updated ListFilePaths to accept a list of file extensions
    Signed-off-by: Frederico Araujo <frederico.araujo@ibm.com>

--- go/ioutils/ioutils.go
+++ go/ioutils/ioutils.go
@@ -29,32 +29,35 @@ import (
 // ListFilePaths lists file paths with extension fileExt in path if
 // path is a valid directory, otherwise, it returns path if path is
 // a valid path and has extension fileExt.
-func ListFilePaths(path string, fileExt string) ([]string, error) {
+func ListFilePaths(path string, fileExts ...string) ([]string, error) {
 	var paths []string
 	if fi, err := os.Stat(path); os.IsNotExist(err) {
 		return paths, err
 	} else if fi.IsDir() {
-		var files []os.FileInfo
-		var err error
-		if files, err = ioutil.ReadDir(path); err != nil {
-			return paths, err
-		}
-		for _, file := range files {
-			if filepath.Ext(file.Name()) == fileExt {
-				f := path + "/" + file.Name()
-				paths = append(paths, f)
+		for _, fileExt := range fileExts {
+			var files []os.FileInfo
+			var err error
+			if files, err = ioutil.ReadDir(path); err != nil {
+				return paths, err
+			}
+			for _, file := range files {
+				if filepath.Ext(file.Name()) == fileExt {
+					f := path + "/" + file.Name()
+					paths = append(paths, f)
+				}
 			}
 		}
-		return paths, nil
 	} else {
-		if filepath.Ext(path) == fileExt {
-			return append(paths, path), nil
+		for _, fileExt := range fileExts {
+			if filepath.Ext(path) == fileExt {
+				return append(paths, path), nil
+			}
 		}
-		return paths, nil
 	}
+	return paths, nil
 }
 
-//FileExists checks whether a file exists and whether it is a directory.
+// FileExists checks whether a file exists and whether it is a directory.
 func FileExists(filename string) (bool, bool) {
 	info, err := os.Stat(filename)
 	if os.IsNotExist(err) {
